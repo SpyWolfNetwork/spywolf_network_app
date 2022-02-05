1d6cd398c1b7cb8d3380ccd77e8233b000aeb9d3
Wil <joswilder46@gmail.com>
2022-02-05 14:57:36 -0300

    Add regex validation to email

--- src/modules/get-audited/components/audit-form/audit-form.tsx
+++ src/modules/get-audited/components/audit-form/audit-form.tsx
@@ -151,10 +151,13 @@ const AuditForm: React.FC = () => {
         setSmartContractAddress(e);
     }
 
+    // eslint-disable-next-line no-control-regex
+    const emailpattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    
     return <Container>
         <Form onChange={formChange} form={auditForm} preserve={false} >
             <div className="inline">
-                <Form.Item label={'Your Email'} extra="*Your invoice will be sent here." name={'email'}  >
+                <Form.Item label={'Your Email'} extra="*Your invoice will be sent here." name={'email'} rules={[{ pattern: emailpattern, message: 'Please enter a valid email' }]}  >
                     <Input placeholder='jon@gmail.com'></Input>
                 </Form.Item>
                 <Form.Item label={'Your Telegram'} name={'telegram'} rules={[{ required: true }]}>
