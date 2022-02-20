611de077e57a50ab90ee8f90c2739ef599b36ac9
Wil <joswilder46@gmail.com>
2022-02-20 17:13:32 -0300

    feat: add partner, fix: edit audit prices and remove twitter handle

--- src/modules/home/components/official-partners/official-partners.tsx
+++ src/modules/home/components/official-partners/official-partners.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { Container } from './official-partners.style';
 import pinksale from '../../../../assets/ads/pinksale-logo.png'
+import travladd from '../../../../assets/ads/travladd-logo.png'
 const OfficialPartners: React.FC<{ title?: string }> = (props) => {
     useEffect(() => { }, []);
 
@@ -15,6 +16,8 @@ const OfficialPartners: React.FC<{ title?: string }> = (props) => {
         <a href='https://www.pinksale.finance/#/' target={'__blank'}><img width={100} src={pinksale} alt="" /></a>
         <a href='https://www.busdx.com/' target={'__blank'}><img width={90} src="https://uploads-ssl.webflow.com/616982c1e07d813c4c2af373/61698b621d30071a9373952d_FullLogo_Transparent_NoBuffer(1)-p-500.png" alt="" /></a>
         <a href='https://cookiesale.app/' target={'__blank'}><img width={120} src="https://cookiesale.app/wp-content/uploads/2022/01/Logo-Dark-v1.svg" alt="" /></a>
+        <a href='https://twitter.com/OfficialTravlad' target={'__blank'}><img width={100} src={travladd} alt="" /></a>
+
     </Container>;
 };
 
